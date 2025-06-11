2965520c07f8bc3380e083d90362b0859b18dc6f
msz <mszczodrak@users.noreply.github.com>
2025-06-11 07:21:22 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import Experience from "../components/Experience";
 import Industry from "../components/Industry";
 import Academia from "../components/Academia";
 import Firebase from "../components/Firebase";
