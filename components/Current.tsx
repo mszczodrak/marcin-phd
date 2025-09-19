10ccad331797f97811ed70126f920e56e438b829
Marcin Szczodrak <msz@microever.com>
2025-09-19 15:40:40 -0700

    current

--- components/Current.tsx
+++ components/Current.tsx
@@ -28,9 +28,15 @@ const currentProjects = [
   },
   {
     title: "VoxQuery",
-    description: "Query your data with your voice. An intuitive way to interact with your databases.",
+    description: "Voice-enabled surveying for rapid data collection and analysis.",
     image: "/images/file_audio_icon_white.webp",
     link: "https://voxquery.com",
+  },
+  {
+    title: "Firmware Studio",
+    description: "Digital-Twin for Cyber-Physical Systems.",
+    image: "/images/firmware_studio_square.webp",
+    link: "https://firmware.studio/",
   },
 ];
 
@@ -53,11 +59,11 @@ const Current = () => {
             {currentProjects.map((project, index) => (
               <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
                 <div className="p-1 h-full">
-                  <Link href={project.link} target="_blank" rel="noopener noreferrer" className="block h-full">
+                  <Link href={project.link} target="_blank" rel="noopener noreferrer" className="block h-full no-underline">
                     <Card className="bg-zinc-800/50 border-zinc-700 hover:border-primary/80 transition-colors h-full">
                       <CardContent className="flex flex-col items-center justify-start text-center p-6 gap-4 h-full">
                         <Image src={project.image} alt={project.title} width={150} height={150} className="rounded-lg shadow-lg" />
-                        <h3 className="text-2xl font-bold text-white">{project.title}</h3>
+                        <h3 className="text-2xl font-bold text-white hover-lime-scale">{project.title}</h3>
                         <p className="text-zinc-300 flex-grow">{project.description}</p>
                       </CardContent>
                     </Card>
